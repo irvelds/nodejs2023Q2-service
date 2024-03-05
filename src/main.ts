288a1b6c314cbd90d5759efdcbe8d52e965c57ea
irvelds <irvelds@mail.ru>
2024-03-05 12:51:28 +0300

    feat: add required dependencies and swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
+import * as yaml from 'js-yaml';
+import { readFile } from 'fs/promises';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const yamlDocument = yaml.load(
+    await readFile('./doc/api.yaml', { encoding: 'utf8' }),
+  ) as OpenAPIObject;
+
+  SwaggerModule.setup('doc', app, yamlDocument);
+
   await app.listen(4000);
 }
+
 bootstrap();
